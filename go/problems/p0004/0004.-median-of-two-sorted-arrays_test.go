047feebd8849a2a5bfded16fcbdfca4a4235e701
kawakattsun <riverwin.akira@gmail.com>
2020-06-26 10:42:46 +0900

    add test case to problem 0004

--- go/problems/p0004/0004.-median-of-two-sorted-arrays_test.go
+++ go/problems/p0004/0004.-median-of-two-sorted-arrays_test.go
@@ -2,12 +2,21 @@ package p0004
 
 import "testing"
 
+type Case struct {
+	Input1   []int
+	Input2   []int
+	Expected float64
+}
+
 func TestFindMedianSortedArrays(t *testing.T) {
-	nums1 := []int{1, 3}
-	nums2 := []int{2}
-	expected := 2.0
-	output := findMedianSortedArrays(nums1, nums2)
-	if output != expected {
-		t.Fatalf("test failed. output: %f", output)
+	cases := []Case{
+		Case{Input1: []int{1, 3}, Input2: []int{2}, Expected: 2.0},
+		Case{Input1: []int{1, 2}, Input2: []int{3, 4}, Expected: 2.5},
+	}
+	for _, c := range cases {
+		output := findMedianSortedArrays(c.Input1, c.Input2)
+		if output != c.Expected {
+			t.Fatalf("test failed. output: %f", output)
+		}
 	}
 }
